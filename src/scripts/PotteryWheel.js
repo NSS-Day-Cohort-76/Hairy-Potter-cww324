79e0c42be7662ba36af65f7f6766fcbd553ade05
Charlie Wunderlich <cww324@gmail.com>
2025-03-14 13:04:43 -0500

    Added potterywheel.js and created pottery object in main.js

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,15 @@
+
+
+
+let currentId = 1;
+
+export const makePottery = (shape, weight, height) => {
+    const pottery = {
+        shape: shape,
+        weight: weight,
+        height: height,
+        id: currentId
+    }
+    currentId++
+    return pottery;
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
-// Imports go first
+import { makePottery } from "./PotteryWheel.js";
 
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("mug", 4, 8)
+let platter = makePottery("Platter", 5, 7);
+let vase = makePottery("Vase", 3, 9);
+let bowl = makePottery("Bowl", 2, 6);
+let pitcher = makePottery("Pitcher", 4, 10);
 
+console.log(mug)
+console.log(platter)
+console.log(vase)
+console.log(bowl)
+console.log(pitcher)
 
+console.log("hello from main.js yo youy there???");
 // Fire each piece of pottery in the kiln
 
 
